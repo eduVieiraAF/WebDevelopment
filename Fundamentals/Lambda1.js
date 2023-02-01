ed71ae036f6e406e8d546806f63b38747d66e6c0
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-01 14:58:48 -0300

    more examples

--- Fundamentals/Lambda1.js
+++ Fundamentals/Lambda1.js
@@ -5,13 +5,23 @@
         else console.log(a-b);
     };
     var div = (a, b) => {
-        if (b <=0 ) console.log("Cannot be divided by ZERO")
-        else console.log(a / b)
+        if (b <=0 ) console.log("Cannot be divided by ZERO");
+        else console.log(a / b);
     };
-    var times = (a, b) => { console.log(a * b) }
+    var times = (a, b) => { console.log(a * b) };
+
+    var typeOf = (it) => { console.log(typeof it) };
 }
 
 sum(5, 6);
 sub(4, 9);
 div(15, 0);
 times(8, 2);
+
+let _string = "Hello";
+let _int = 4
+let _boolean = true
+
+typeOf(_string);
+typeOf(_int);
+typeOf(_boolean);
