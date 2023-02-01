8c1f52041f7328e46291b9dc8e6b04ea81d89102
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-01 15:51:25 -0300

    more on lambdas

--- Fundamentals/Lambda1.js
+++ Fundamentals/Lambda1.js
@@ -1,5 +1,5 @@
 {
-    var sum = (a,b) => { console.log(a + b) };
+    var sum = (a,b) => console.log(a + b) ;
     var sub = (a, b) => {
         if (a < b) console.log(`1st number is smaller → ${ a-b }`);
         else console.log(a-b);
@@ -8,9 +8,10 @@
         if (b <=0 ) console.log("Cannot be divided by ZERO");
         else console.log(a / b);
     };
-    var times = (a, b) => { console.log(a * b) };
+    var times = (a, b) => console.log(a * b);
 
-    var typeOf = (it) => { console.log(typeof it) };
+    var typeOf = (it) => console.log(typeof it);
+
 }
 
 sum(5, 6);
