c93ba8c02ac65536c0c12a014c9c9738fe115c32
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-03 10:50:13 -0300

    ternay

---
+++ Fundamentals/ternary.js
@@ -0,0 +1,7 @@
+const result = mark => mark >= 7 ? "Aproved" : "Failed"
+// *                            (if)        true      else
+
+console.log(result(6.8))
+console.log(result(7.1))
+console.log(result(4.5))
+console.log(result(9.0))
