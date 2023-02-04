461606b3fdc18fc04dbcb015713123085972af52
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-04 16:14:00 -0300

    more for

--- Fundamentals/for.js
+++ Fundamentals/for.js
@@ -7,3 +7,18 @@ const marks = [7, 8, 9, 6, 10]
 for (i = 0; i < marks.length; i++) {
     console.log(`Grade → ${ marks[i] }`)
 }
+
+for (let i in marks) {
+    console.log(marks[i])
+}
+
+const person = {
+    name: "Edu",
+    age: "40",
+    height: 1.70,
+    weight: 79
+}
+
+for (let item in person) {
+    console.log(`${ item } → ${ person[item] }`)
+}
