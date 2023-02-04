580b48944687339e18309888c24c4210f57051b7
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-04 16:57:38 -0300

    functions 1

---
+++ Functions/Example01.js
@@ -0,0 +1,39 @@
+// * First-class Object (Citizen)
+// * Higher-order function
+
+// * Standard way
+function fun1(data) {}
+
+//* Storing a function in a variable
+const fun2 = function(data) {}
+
+//* storing inside an array
+const array = [
+    function sum(a, b) { return a + b },
+    fun1,
+    fun2
+]
+
+console.log(array[0](5, 45))
+
+//* Passing a function as a parameter
+function run (fun) {
+    fun()
+}
+
+run(function() { console.log("Running...") })
+
+//* Passing as an object attribute
+const obj = {}
+obj.greet = function (name) { return `Hello ${ name }` }
+
+console.log(obj.greet("Edu"))
+
+//* May return/contain a function
+function add(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+
+add(1, 2)(3)
