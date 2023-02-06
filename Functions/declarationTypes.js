6d2f4a3fcd53d1a0831ea0db3bb1eebab0037133
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-06 10:58:53 -0300

    types of declaration

---
+++ Functions/declarationTypes.js
@@ -0,0 +1,10 @@
+// function declaration
+function fun1(x, y) { return x + y }
+
+// function expression
+const fun2 = function (x, y) { return x + y }
+
+// named function expression
+const fun3 = function fun3(x, y) { return x + y }
+        // not commonly used
+        // can be handy when debugging
