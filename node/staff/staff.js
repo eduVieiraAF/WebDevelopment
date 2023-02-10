fd1b482f8d7ec870b0081a03b55336dfd1f1360c
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-10 16:08:01 -0300

    request and axios
    to determine smallest salary for a chinesse woman

---
+++ node/staff/staff.js
@@ -0,0 +1,24 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chinese = f => f.pais === "China"
+const females = f => f.genero === "F"
+const smallestSalary = (members, curMember) => {
+    return members.salario < curMember.salario ? members : curMember
+}
+
+
+axios.get(url).then(response => {
+    const staff = response.data
+
+    console.log(staff)
+
+    // Chinese woman with the smallest salary
+    const funStaff = staff
+        .filter(chinese)
+        .filter(females)
+        .reduce(smallestSalary)
+    
+        console.log(funStaff)
+
+})
