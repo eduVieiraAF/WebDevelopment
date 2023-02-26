73fd5f88abe1c8b2a69cca2e8c5d61a94db3dd3a
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-26 11:00:08 -0300

    changes

--- Practice/workData.js
+++ Practice/workData.js
@@ -2,4 +2,5 @@ const products = require('./SRAgricula.json')
 
 console.log(products['2166-2366'][8].CORREIAS.concat(" - ", products['2166-2366'][8].__EMPTY_2))
 
-console.log(products['2388 - 01'][3].__EMPTY_2)
+console.log(products['2388 - 01'][3].CORREIAS.concat(' - ', products['2388 - 01'][3].__EMPTY_2))
+
