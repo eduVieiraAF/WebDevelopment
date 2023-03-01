4f07f4dfe345fd71c8e8fe94d39afb5d4e4de946
Eduardo Vieira <edu_vieira@hotmail.com>
2023-02-28 23:01:45 -0300

    work thing

--- Practice/workData.js
+++ Practice/workData.js
@@ -1,6 +1,30 @@
 const products = require('./SRAgricula.json')
+const fs = require('fs')
 
-console.log(products['2166-2366'][8].CORREIAS.concat(' - ', products['2166-2366'][8].__EMPTY_2))
+const prodSortedDescription = []
+const rejects = []
+const jsonKeys = Object.keys(products)
 
-console.log(products['2388 - 01'][3].CORREIAS.concat(' - ', products['2388 - 01'][3].__EMPTY_2))
+for (let key of jsonKeys) {
+    for (let i = 0; i < products[key].length; i++) {
+       if (!products[key][i].CORREIAS || !products[key][i].__EMPTY_2) rejects.push(products[key][i])
+       else prodSortedDescription.push(products[key][i].CORREIAS.toString().trim().concat('-', products[key][i].__EMPTY_2))
+    }
+}
+
+const prodObject = []
+let NCM = 0
+
+prodSortedDescription.forEach((description, index) => {
+    prodObject[index] = {
+        ncm: NCM.toString().padStart(6, '0'),
+        description,
+        un: "UN"
+    }
+
+    NCM++
+})
+
+const csvForm = prodObject.map(row => Object.values(row).join(',')).join('\n')
+fs.writeFileSync("D:/WebDevelopment/Practice/SRAgricula.csv", csvForm)
 
