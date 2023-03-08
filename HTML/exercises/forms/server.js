b121ef0b5bfee693d4ebe0b4ff7d26111b6c0064
Eduardo Vieira <edu_vieira@hotmail.com>
2023-03-08 14:18:20 -0300

    forms pt. 3

--- HTML/exercises/forms/server.js
+++ HTML/exercises/forms/server.js
@@ -6,7 +6,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/users', (req, res) => {
     console.log(req.body)
-    res.send("<h2>Hooray</h2>")
+    res.send("<h2>Hooray. New user is in!</h2>")
+
+})
+
+app.post('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send("<h2>Alterations successful</h2>")
 })
 
 app.listen(3003)
