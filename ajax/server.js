9d61ab719739cb3bf234015c1dfa2e0e75fa22be
Eduardo Vieira <edu_vieira@hotmail.com>
2023-04-29 16:49:11 -0300

    request1

--- ajax/server.js
+++ ajax/server.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 //* Middlewares
-app.use(express.static('.'))
+app.use(express.static('.')) // provides static content (files, etc)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/test', (req, res) => res.send("all good here"))
+app.get('/test', (req, res) => res.send(`all good here → ${ new Date }`))
 app.listen(8080, () => console.log('Server running...'))
