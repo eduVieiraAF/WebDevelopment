e9b0fdd5efc3e5c735086b47e15853732b841e9a
Eduardo Vieira <edu_vieira@hotmail.com>
2023-05-03 20:16:24 -0300

    gulp pt2

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,11 +1,38 @@
 const gulp = require('gulp')
-//const { series } = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
+
 
 function copying(callback) {
-    console.log('Copying task')
+    //console.log('Copying task')
+    //gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        // .pipe(transform()) create a 'pipeline' to work the files
+        // .pipe(renderBW())
+        .pipe(gulp.dest('pastaB'))
 
     return callback()
 }
 
-module.exports.default = series(copying)
+const before1 = callback => {
+    console.log("A task before #1")
+
+    return callback()
+
+}
+const before2 = callback => {
+    console.log("A task before #2")
+
+    return callback()
+}
+
+const end = callback => {
+    console.log("The end")
+
+    return callback()
+}
+
+module.exports.default = series(
+    parallel(before1, before2),
+    copying,
+    end
+)
