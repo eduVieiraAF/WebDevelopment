ee0d7e97d2e90b090387e7115c508b51d90f8d46
Eduardo Vieira <edu_vieira@hotmail.com>
2023-05-06 20:19:30 -0300

    larger gulp scenario

---
+++ gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+
+
+function monitorFiles(cb) {
+
+    return cb()
+}
+
+function server(cb) {
+
+    return cb()
+}
+
+module.exports = {
+    monitorFiles,
+    server
+}
