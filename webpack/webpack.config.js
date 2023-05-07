c929f6a5755428829bb9bab366fe52069d53f316
Eduardo Vieira <edu_vieira@hotmail.com>
2023-05-07 15:03:25 -0300

    adding output  config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,5 +2,9 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    entry: './src/main.js'
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/public'
+    }
 }
