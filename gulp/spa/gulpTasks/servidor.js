fc986a8434aa1304efe33234f0891428ef4caeb7
Eduardo Vieira <edu_vieira@hotmail.com>
2023-05-07 12:26:32 -0300

    gulp w/ server

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,15 +1,27 @@
 const gulp = require('gulp')
+const webServer = require('gulp-webserver')
+const watch = require('gulp-watch')
 
+function server() {
+
+    return gulp.src('build')
+        .pipe(webServer({
+            port: 8080,
+            open: true,
+            livereload: true
+        }))
+}
 
 function monitorFiles(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.scss', () => gulp.series('appCss')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src//assets/imgs/**/*.*', () => gulp.series('appHTML')())
+
 
     return cb()
 }
 
-function server(cb) {
-
-    return cb()
-}
 
 module.exports = {
     monitorFiles,
