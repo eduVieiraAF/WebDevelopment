9f01f8ea4d84ddffbcc89ee752299d09e991e501
Eduardo Vieira <edu_vieira@hotmail.com>
2023-05-13 10:18:39 -0300

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,30 @@
+const modoDev = process.env.NODE_ENV !== "production"
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptmizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/main.js',
     output: {
         filename: 'main.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimizer: new OptmizeCSSAssetsPlugin({})
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'style.css'
-        })
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
     ],
     module: {
         rules: [{
