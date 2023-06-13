a5a2bf311d6927d6a7b7a2162d382a701e881aa0
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-12 22:27:16 -0300

    more examples with 2+ elements

--- react2/exercicios/src/components/BomDia.jsx
+++ react2/exercicios/src/components/BomDia.jsx
@@ -8,7 +8,21 @@ import React from "react";
 //   </ React.Fragment>
 // );
 
-export default (props) => [
-  <h1 key='greetName'>Bom dia, {props.nome}! </h1>,
-  <h2 key='age'>Sua idade é {props.idade}</h2>,
-];
+// export default props => (
+//   <React.Fragment>
+//     <h1>Bom dia, {props.nome}! </h1>
+//     <h2>Sua idade é {props.idade}</h2>
+//   </ React.Fragment>
+// );
+
+export default props => (
+  <div>
+    <h1>Bom dia, {props.nome}! </h1>
+    <h2>Sua idade é {props.idade}</h2>
+  </ div>
+);
+
+// export default (props) => [
+//   <h1 key='greetName'>Bom dia, {props.nome}! </h1>,
+//   <h2 key='age'>Sua idade é {props.idade}</h2>,
+// ];
