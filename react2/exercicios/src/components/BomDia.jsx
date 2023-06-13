0ada35eaa4649346d84bbd6dd03a136b261c3ada
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-12 22:25:02 -0300

    components with params

---
+++ react2/exercicios/src/components/BomDia.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+// export default props => (
+//   <React.Fragment>
+//     <h1>Bom dia, {props.nome}! </h1>
+//     <h2>Sua idade é {props.idade}</h2>
+//   </ React.Fragment>
+// );
+
+export default (props) => [
+  <h1 key='greetName'>Bom dia, {props.nome}! </h1>,
+  <h2 key='age'>Sua idade é {props.idade}</h2>,
+];
--- react2/exercicios/src/index.js
+++ react2/exercicios/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro from './components/Primeiro'
+// import Primeiro from './components/Primeiro'
+import BomDia from './components/BomDia'
 
-const title1 = <Primeiro />
+// const title1 = <Primeiro />
+const bomDia = <BomDia nome="Eduardo" idade="41"/>
 const root = document.getElementById('root')
 
-ReactDOM.render(title1, root)
+ReactDOM.render(bomDia, root)
