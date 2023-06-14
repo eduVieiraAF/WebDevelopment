bbf934ee87484fc6ebfba7bef96973d9f7ac3ecf
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-13 21:37:53 -0300

    parent & children

--- react2/exercicios/src/index.js
+++ react2/exercicios/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 // import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
 // import { BoaTarde, BoaNoite } from './components/Multiplos'
-import Saudacao from './components/Saudacao'
+// import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
 
 // const title1 = <Primeiro />
@@ -11,8 +13,11 @@ import Saudacao from './components/Saudacao'
 const root = document.getElementById('root')
 
 ReactDOM.render(
-    <div>
-       <Saudacao tipo="Bom dia" nome="Edu"></Saudacao>
-    </div>
-    , root
-    )
+   <div>
+      <Pai nome="Edu" sobrenome="Vieira">
+         <Filho nome="Daniel"></Filho>
+         <Filho nome="Sofia" sobrenome="Vieira"></Filho>
+      </Pai>
+   </div>
+   , root
+)
