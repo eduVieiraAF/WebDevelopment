a037780bf08eb8a1fb93c9ddbf53e5870e294ca8
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-13 21:00:32 -0300

    2 ways to handle onChange

--- react2/exercicios/src/components/Saudacao.jsx
+++ react2/exercicios/src/components/Saudacao.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 export default class Saudacao extends Component {
+  
   state = {
     tipo: this.props.tipo,
     nome: this.props.nome,
   };
+  
+  constructor(props) {
+    super(props)
+    
+    // option 1 for the onChange
+    this.setTipo = this.setTipo.bind(this)
+  }
 
   setTipo(e) {
     // console.log(e.target.value)
@@ -28,13 +36,13 @@ export default class Saudacao extends Component {
           type="text"
           placeholder="Tipo..."
           value={tipo}
-          onChange={(e) => this.setTipo(e)}
+          onChange={this.setTipo}
         />
         <input
           type="text"
           placeholder="Nome..."
           value={nome}
-          onChange={(e) => this.setNome(e)}
+          onChange={e => this.setNome(e)}
         />
       </>
     );
