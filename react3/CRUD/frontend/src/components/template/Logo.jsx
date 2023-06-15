6ff46edc63c9404eaf6b9294d4e6fca779306b8d
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-15 19:19:10 -0300

    css grid

--- react3/CRUD/frontend/src/components/template/Logo.jsx
+++ react3/CRUD/frontend/src/components/template/Logo.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './Logo.css'
 
 
-export default props => {
-    <aside className="logo">Logo</aside>
-}
+export default props => 
+    <aside className="logo">
+        Logo
+    </aside>
--- react3/CRUD/frontend/src/main/App.jsx
+++ react3/CRUD/frontend/src/main/App.jsx
@@ -2,10 +2,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import './App.css'
+import Logo from "../components/template/Logo";
 import Main from "../components/template/Main";
 import Nav from "../components/template/Nav";
 import Footer from "../components/template/Footer";
-import Logo from "../components/template/Logo";
 
 export default props => 
     <div className="app">
