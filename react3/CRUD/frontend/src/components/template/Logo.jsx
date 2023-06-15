116c7055671101cae43689179dc9e05469120103
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-15 19:30:19 -0300

    logo

--- react3/CRUD/frontend/src/components/template/Logo.jsx
+++ react3/CRUD/frontend/src/components/template/Logo.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 import './Logo.css'
-
+import logo from '../../assets/imgs/logo.png'
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
