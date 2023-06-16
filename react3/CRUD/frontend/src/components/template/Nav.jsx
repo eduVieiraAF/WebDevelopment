a89d6010103b0fa80f0ae6bf8be2e2dae25c93aa
Eduardo Vieira <edu_vieira@hotmail.com>
2023-06-16 06:58:37 -0300

    nav

--- react3/CRUD/frontend/src/components/template/Nav.jsx
+++ react3/CRUD/frontend/src/components/template/Nav.jsx
@@ -3,4 +3,13 @@ import React from "react";
 import './Nav.css'
 
 export default props => 
-    <aside className="menu-area">Navigation</aside>
+    <aside className="menu-area">
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Start
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"> Users</i>
+            </a>
+        </nav>
+    </aside>
