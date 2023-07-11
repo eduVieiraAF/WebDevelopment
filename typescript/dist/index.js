84844c1c0a801b07e136cf41aacaf35f5f07528d
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-07-11 13:05:28 -0300

    trial

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const data_json_1 = __importDefault(require("./data/data.json"));
 console.log("Adding a bit of TS");
+console.log(data_json_1.default.data.split("."));
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,6 @@
+import data from './data/data.json'
 console.log("Adding a bit of TS")
+
+console.log(data.data.split("."))
+
 
