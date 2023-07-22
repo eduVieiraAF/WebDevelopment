d27e2e052718d3bb0479eaab765b4a0c69d338ea
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-07-22 10:16:46 -0300

    testing

---
+++ JavaScript/tests/extract.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const papaParse = require("papaparse")
+
+const deptArrays = {}
+
+fs.readFile('./ccontabil.csv', 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    const parseData = papaParse.parse(data, {
+        header: true,
+        skipEmptyLines: true
+    })
+
+    parseData.data.forEach((row) => {
+        Object.keys(row).forEach((key) => {
+            if (key != '') {
+                if (!deptArrays[key]) {
+                    deptArrays[key] = []
+                }
+
+                deptArrays[key].push(row[key].toString())
+            }
+        })
+    })
+})
+
+
+function saveToJSON(data) {
+    //console.log(data)
+
+    const jsonData = JSON.stringify(data)
+    fs.writeFile('data.json', jsonData, { encoding: 'utf8' }, (err) => {
+        if (err) console.log(err)
+        else console.log("Done")
+    })
+}
+
