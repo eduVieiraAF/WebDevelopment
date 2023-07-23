d632fba7336ce1c4d749a6a2b039ad1cc7949465
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-07-23 12:36:31 -0300

    game finished

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
     <h1>The Monty Hall problem</h1>
-    <Door number="1" :hasGift="true"/>
+    <!-- <Door number="1" :hasGift="false"/> -->
+    <div class="form">
+      <div v-if="!started">
+        <label for="doors">How many doors?</label>
+        <input
+          type="text"
+          id="doors"
+          size="3"
+          style="margin-left: 7px"
+          v-model.number="doors"
+        />
+      </div>
+      <div v-if="!started" style="margin-top: 15px; ">
+        <label for="selectedDoor">Which doors has a gift?</label>
+        <input
+          type="text"
+          id="selectedDoor"
+          size="3"
+          style="margin-left: 7px"
+          v-model.number="selectedDoor"
+        />
+      </div>
+      <button v-if="!started" @click="started = true" style="margin-top: 17px">BEGIN</button>
+      <button v-if="started" @click="started = false">Go AGAIN</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in doors" :key="i">
+        <Door :hasGift="i === selectedDoor" :number="i"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Door from './components/Door.vue';
+import Door from "./components/Door.vue";
 // import Gift from "./components/Gift.vue";
 
 export default {
   name: "App",
   components: { Door },
+  data: function () {
+    return {
+      started: false,
+      doors: 3,
+      selectedDoor: null,
+    };
+  },
 };
 </script>
 
@@ -39,5 +75,26 @@ body {
   box-shadow: 1px 1px 6px black;
   background: linear-gradient(to left, rgb(4, 63, 63), rgb(33, 25, 83));
   margin-bottom: 60px;
+}
+.form {
+  display: flex;
+  font-size: 1.2em;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to left, rgb(4, 63, 63), rgb(33, 25, 83));
+  border: 1px solid darkseagreen;
+  background-color: #0000;
+  padding: 18px;
+  border-radius: 20px;
+  box-shadow: 1px 1px 6px black;
+  margin-bottom: 40px;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
