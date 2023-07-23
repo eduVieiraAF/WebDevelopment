28687d06f2f1dab5bac434d5263f943cd70594a1
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-07-23 17:15:03 -0300

    new project desktop application

---
+++ desktop/most-used-words/src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pills">
+    <PillComponent v-for="it in groupedWords" :word="it.word" :amount="it.amount" :key="it.word"/>
+  </div>
+</template>
+
+<script>
+import PillComponent from './PillComponent.vue';
+
+
+export default {
+    components: { PillComponent },
+    data: function() {
+        return {
+            groupedWords: [
+                // mocked data
+                { word: "i", amount: 547 },
+                { word: "you", amount: 478 },
+                { word: "it", amount: 110 }
+            ]
+        }
+    }
+};
+</script>
+
+<style scoped>
+.pills {
+    display: flex;
+}
+</style>
---
+++ desktop/most-used-words/src/components/PillComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pill">
+    <span class="word">{{ word }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["word", "amount"],
+};
+</script>
+
+<style scoped>
+.pill {
+  margin: 10px;
+  height: 55px;
+  width: fit-content;
+  border-radius: 40px;
+  color: blanchedalmond;
+  font-size: 1.3rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  background-color: firebrick;
+}
+
+.pill .word {
+  margin-left: 15px;
+}
+
+.pill .amount {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: blanchedalmond;
+  color: firebrick;
+  margin: 10px;
+  padding: 5px;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
