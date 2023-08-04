ecd9531c84f8623c56f36e7621242576ef1795b1
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-04 08:06:13 -0300

    nav + home

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -11,7 +11,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 
 import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
 
 
 @NgModule({
@@ -20,6 +22,7 @@ import { MatListModule } from '@angular/material/list'
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
