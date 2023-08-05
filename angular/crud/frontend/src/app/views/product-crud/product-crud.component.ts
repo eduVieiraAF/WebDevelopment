3be557049d68bd320f105b24e456cfd43c00e3a9
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-04 21:14:51 -0300

    model + routes

--- angular/crud/frontend/src/app/views/product-crud/product-crud.component.ts
+++ angular/crud/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent {
+  constructor(private router: Router) {}
 
+  navigateToProductCreate() {
+    this.router.navigate(['/products/create'])
+  }
 }
