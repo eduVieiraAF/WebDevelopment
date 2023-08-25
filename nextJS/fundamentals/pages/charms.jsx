0ddfe1d8341968d57f284cd68772c6ec5d6e16a9
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-25 14:42:02 -0300

    fundamentals

---
+++ nextJS/fundamentals/pages/charms.jsx
@@ -0,0 +1,9 @@
+import styles from './charms..module.css'
+
+export default function Charms() {
+    return (
+        <div>
+            <h1 className= {styles.cherry}>individual charm</h1>
+        </div>
+    )
+}
