88198c8f4a556d4859c5bdf1dc7f4e9815a932a2
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-25 15:27:33 -0300

    properties in components

--- nextJS/fundamentals/components/Header.jsx
+++ nextJS/fundamentals/components/Header.jsx
@@ -1,7 +1,12 @@
-export default function Header() {
+export default function Header(props) {
+console.log(`${props.title} → properties: ${JSON.stringify(props)}`)
+// * props is read-only
+
     return (
         <header>
             <h1>Next.js fundamentals</h1>
+            <h2>{ props.title }</h2>
+            <h2>{ props.name }</h2>
         </header>
     )
 }
--- nextJS/fundamentals/pages/sample.jsx
+++ nextJS/fundamentals/pages/sample.jsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 export default function Sample() {
     return (
         <div>
-            <Header />
+            <Header name="sample" age={30} isActive={true} title="Next.js & React"/>
             <p>more than one element</p>
         </div>
     )
