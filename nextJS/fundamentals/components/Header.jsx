739a4e21037bf54f758d85bf46105a12daae8a97
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-25 14:48:59 -0300

    components + fragments

---
+++ nextJS/fundamentals/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Next.js fundamentals</h1>
+        </header>
+    )
+}
---
+++ nextJS/fundamentals/pages/sample.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Sample() {
+    return (
+        <div>
+            <Header />
+            <p>more than one element</p>
+        </div>
+    )
+}
